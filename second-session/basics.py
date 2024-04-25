28e37ff831d18c17ba8ccc02fbab2d698549955c
engAbdullahHanif <engabdullahhanif@gmail.com>
2024-04-25 19:05:40 +0200

    second session2

--- second-session/basics.py
+++ second-session/basics.py
@@ -20,8 +20,53 @@ my_character = 'c'
 my_boolean = True
 my_float = 4.5
 
-print(my_number)
-print(my_string)
-print(my_character)
-print(my_boolean)
-print(my_float)
+
+
+# print(my_number)
+# print(my_string)
+# print(my_character)
+# print(my_boolean)
+# print(my_float)
+
+# pulling github repository
+# git clone https://github.com/EngAbdullahHanif/python-class.git
+
+# git pull origin main
+
+
+# control flow
+# if else statement
+
+num = 3
+
+# >, <, >=, <=, ==, !=
+
+# if num > 5:
+#     print("Number is greater than 5")
+#     print("Hello, World!")
+#     print("Python")
+#     print("Java")
+# else: 
+#     print("Number is less than 5")
+
+# loops
+# for loop
+# while loop
+
+# print("For Loop1")
+range_value = range(5)
+# print("range_value", range_value)
+
+my_list = (1, 2, 3, 4, 5)
+
+# print("my_list", my_list)
+
+for i in range(10):
+    print('Hello, World!')
+    
+
+
+
+
+
+
