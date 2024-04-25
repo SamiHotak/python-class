abaaaf807abe3a1abbf3f2eebb4978eb1bf42e13
engAbdullahHanif <engabdullahhanif@gmail.com>
2024-04-25 18:30:54 +0200

    second session

---
+++ second-session/basics.py
@@ -0,0 +1,27 @@
+# print statment
+
+# no braces, no semicolons
+
+# print("Hello, World!")
+
+# data types in python 
+# number (integer, float) 
+# 5, 6, 9.5, 34.5
+
+# string (character, text)
+# 'c', 'a',  "Hello, World!", "Python"
+
+# boolean (True, False)
+# True, False
+
+my_number = 4
+my_string = "Hello, World!"
+my_character = 'c'
+my_boolean = True
+my_float = 4.5
+
+print(my_number)
+print(my_string)
+print(my_character)
+print(my_boolean)
+print(my_float)
